6fe6833cfc759a2a82048df083facbc849b6518b
Elena Carey <theelenacarey@gmail.com>
2022-06-01 10:55:53 -0500

    Completed Error handling. Errors redirect to home page.

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,8 +5,16 @@ var repoNameEl = document.querySelector("#repo-name");
 var getRepoName = function(){
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  getRepoIssues(repoName);
-  repoNameEl.textContent = repoName;
+  
+  if(repoName){
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
+  
+  
 };
 
 var getRepoIssues = function(repo) {
@@ -25,7 +33,7 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          document.location.replace("./index.html");
         }
       });
     
